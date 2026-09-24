0be66dd935b3acce396ad0de2371827d17046894
Tyler <tyler.mckillop18@gmail.com>
2026-09-24 11:23:12 +0100

    Added libraries and secret keys

---
+++ main.py
@@ -0,0 +1,16 @@
+#1) Initialy import all libraries/packages necessary for the script to run
+
+import requests
+
+import os
+
+import json
+
+from datetime import datetime as dt
+import time
+
+import logging
+
+import boto3
+
+from dotenv import load_dotenv
