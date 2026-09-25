fb777bafc8e6730d78c7835e25664673c8eb6a28
Tyler <tyler.mckillop18@gmail.com>
2026-09-25 10:13:34 +0100

    created new zip file changes and began making logging details

--- main.py
+++ main.py
@@ -55,6 +55,21 @@ start_time = yesterday.strftime('%Y%m%dT00')
 end_time = yesterday.strftime('%Y%m%dT23')
 
 
+# Create logging folder and then file path
+
+log_dir = 'log'
+os.makedirs(log_dir),
+
+
+log_filename = f"logs/logging_amplitude_data_{dt.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename=log_filename
+)
+
+
 #5. Create time parameters and then also create response variable using URL variable created earlier
 
 params = {
@@ -70,32 +85,53 @@ status = response.status_code
 
 zip_path = f'{amp_dir}/{timestamp}.zip'
 extract_dir = f'{amp_dir}/{timestamp}'
+
+clean_extract_dir = f'{amp_dir}/{timestamp}/clean'
+os.makedirs(clean_extract_dir, exist_ok = True)
+
 extension = ".json.gz"
+clean_extension = ".json"
+
+#6. While loop and check against status code
+max_retry = 5
+attempt = 0
+delay = 10
+
+while attempt < max_retry:
+
+    if status == 200:
+    #Creating and open the file on disk at zip_path in write-binary mode
+        with open(zip_path, 'wb') as file:     
+            file.write(response.content)        
+        #Write the downloaded raw bytes into that file, this is saving the zip
+
+    #Need to open the zip file and unzip everything inside it into the extract_dir
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:  
+            zip_ref.extractall(extract_dir)   
+        #Sub-folder created and then we
+        #grab that subfolder's full path so we can look inside it
+        gz_folder = os.path.join(extract_dir, os.listdir(extract_dir)[0])
+
+        for item in os.listdir(gz_folder):     #Loop through every filename inside that subfolder
+            if item.endswith(extension):        #Only process files ending in ".json.gz", skip everything else
+                file_path = os.path.join(gz_folder, item)  #Build the full path to this .json.gz file
+                out_path = file_path[:-3]                   #Same path, minus the last 3 chars (".gz"), which then becomes the output filename
+
+                with gzip.open(file_path, 'rb') as f_in:    #Open the .gz file for reading, with gzip decompressing it as we read
+                    with open(out_path, 'wb') as f_out:      #Create the new decompressed output file
+                        shutil.copyfileobj(f_in, f_out)       #Stream the decompressed bytes from f_in into f_out
 
 
-#6. Check against status code
+        for item in os.listdir(gz_folder):
+            if item.endswith(clean_extension):
+                source_path = os.path.join(gz_folder, item)
+                clean_path = os.path.join(clean_extract_dir, item)
+                shutil.move(source_path, clean_path)
+                         
+    else:
+        print(status, response.text)  #Rquest failed, so print the status code and error message for debugging
 
-if status == 200: 
-    with open(zip_path, 'wb') as file:
-        file.write(response.content)
-
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_dir)
-
-    # look inside the project-id folder that extractall created
-    gz_folder = os.path.join(extract_dir, os.listdir(extract_dir)[0])
-
-    for item in os.listdir(gz_folder):
-        if item.endswith(extension):
-            file_path = os.path.join(gz_folder, item)     # full path to the file
-            out_path = file_path[:-3]                     # chop off ".gz"
-            with gzip.open(file_path, 'rb') as f_in:
-                with open(out_path, 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
-            print(out_path)
-else:
-    print(status, response.text)
-
-#new folder with unzipped files is where they save 
+shutil.rmtree(gz_folder)
+os.remove(zip_path)
 #add logging in
 #add true except
